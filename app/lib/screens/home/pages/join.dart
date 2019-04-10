54554cd3c9ae0dd80a0b25e9cf7078384af0ce26
Marcel Herd <me@marcelherd.com>
2019-04-10 19:30:43 +0200

    Simplify JoinScreen

--- app/lib/screens/home/pages/join.dart
+++ app/lib/screens/home/pages/join.dart
@@ -8,42 +8,22 @@ class JoinScreen extends StatelessWidget {
         padding: EdgeInsets.all(16),
         child: Column(
           children: <Widget>[
-            Container(
-              padding: EdgeInsets.only(bottom: 16),
-              child: Text(
-                'Anmeldung',
-                style: DefaultTextStyle.of(context)
-                    .style
-                    .apply(fontSizeFactor: 2.0),
-              ),
-            ),
             TextField(
-              autofocus: false,
               decoration: InputDecoration(
                 border: OutlineInputBorder(),
                 hintText: 'Redecode',
               ),
             ),
-            Row(children: <Widget>[
-              Expanded(
-                child: Padding(
-                  padding: EdgeInsets.only(right: 8),
-                  child: RaisedButton(
-                    child: Text('Enter'),
-                    color: Colors.lightBlue,
-                    onPressed: () {},
-                  ),
-                ),
-                flex: 3,
-              ),
-              Expanded(
-                child: RaisedButton(
-                  child: Text('Re-Enter'),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                RaisedButton(
+                  child: Text('Beitreten'),
+                  color: Colors.lightBlue,
                   onPressed: () {},
                 ),
-                flex: 1,
-              ),
-            ])
+              ],
+            )
           ],
         ),
       ),
