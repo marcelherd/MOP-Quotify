8a2cd68bf2625e045c38544ee4abaed82831900f
Marcel Herd <me@marcelherd.com>
2019-04-10 19:35:33 +0200

    CreateScreen mock

--- app/lib/screens/home/pages/create.dart
+++ app/lib/screens/home/pages/create.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/material.dart';
 
 class CreateScreen extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text('Create'),
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Thema',
+              ),
+            ),
+          ],
+        ),
       ),
+      persistentFooterButtons: <Widget>[
+        FlatButton(
+          child: Text('Erstellen'),
+          onPressed: () {},
+        )
+      ],
     );
   }
-
 }
