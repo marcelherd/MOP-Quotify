e8cb3a325d118de7f75bd6f081c38e0246fb0363
Marcel Herd <me@marcelherd.com>
2019-04-12 10:56:07 +0200

    Sample FireStore

--- app/lib/screens/debate/pages/overview.dart
+++ app/lib/screens/debate/pages/overview.dart
@@ -1,19 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class OverviewScreen extends StatelessWidget {
+  void _onPressAdd() {}
+
+  void _onTapListItem(DocumentSnapshot document) {
+    Firestore.instance.runTransaction((transaction) async {
+      DocumentSnapshot freshSnapshot =
+          await transaction.get(document.reference);
+      await transaction.update(freshSnapshot.reference, {
+        'clicks': freshSnapshot['clicks'] + 1,
+      });
+    });
+  }
+
+  Widget _buildListItem(BuildContext context, DocumentSnapshot document) {
+    return ListTile(
+      title: Row(
+        children: <Widget>[
+          Expanded(
+            child: Text(
+              document['text'],
+              style: Theme.of(context).textTheme.headline,
+            ),
+          ),
+          Text(
+            document['clicks'].toString(),
+            style: Theme.of(context).textTheme.headline,
+          ),
+        ],
+      ),
+      onTap: () => _onTapListItem(document),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        padding: EdgeInsets.all(16),
-        child: Column(
-          children: <Widget>[
-            Text('Overview'),
-          ],
-        ),
-      ),
+          padding: EdgeInsets.all(16),
+          child: StreamBuilder(
+              stream: Firestore.instance.collection('samples').snapshots(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) return Text('Loading...');
+
+                return ListView.builder(
+                  itemExtent: 80.0,
+                  itemCount: snapshot.data.documents.length,
+                  itemBuilder: (context, index) =>
+                      _buildListItem(context, snapshot.data.documents[index]),
+                );
+              })),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: _onPressAdd,
         child: Icon(Icons.add),
       ),
     );
