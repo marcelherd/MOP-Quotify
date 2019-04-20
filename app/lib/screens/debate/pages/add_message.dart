5267643eb60ca0513ed1c5b7b94d27d78767bf3c
David <coregamer89@hotmail.de>
2019-04-20 19:59:57 +0200

    Picker replaced for numberpicker.

--- app/lib/screens/debate/pages/add_message.dart
+++ app/lib/screens/debate/pages/add_message.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_picker/flutter_picker.dart';
+import 'package:numberpicker/numberpicker.dart';
 
 class MessageAdd extends StatefulWidget {
 
@@ -11,12 +11,11 @@ class MessageAdd extends StatefulWidget {
 
 class _MessageAdd extends State<MessageAdd> {
   
-  int _minutes;
+  int _minutes = 1;
   String _messageTitle;
   TextField _textField;
-  Picker _picker;
   SimpleDialog _dialog;
-  ScaffoldState _result;
+  NumberPicker _minutePicker;
 
   _MessageAdd() {
 
@@ -25,7 +24,16 @@ class _MessageAdd extends State<MessageAdd> {
       border: OutlineInputBorder(),
       hintText: 'Meldetitel',
       ),
-      onChanged: (messageTitle) => _messageTitle = messageTitle,
+      onChanged: (messageTitle) => 
+        setState(() => _messageTitle = messageTitle),
+    );
+
+    _minutePicker = NumberPicker.integer(
+      initialValue: _minutes,
+      minValue: 1,
+      maxValue: 10,
+      onChanged: (currentNumber) => 
+        setState(() => _minutes = currentNumber),
     );
 
     _dialog = SimpleDialog(
@@ -33,40 +41,9 @@ class _MessageAdd extends State<MessageAdd> {
       contentPadding: EdgeInsets.fromLTRB(12, 12, 12, 12),
       children: <Widget>[
         _textField,
-        IconButton(
-          icon: Icon(Icons.add),
-          onPressed: () => {
-            _result = context.ancestorStateOfType(const TypeMatcher<ScaffoldState>()),
-          _onShowPicker(context)
-          },
-        )
+        _minutePicker,
       ],
     );
-
-    _picker = new Picker(
-        adapter: NumberPickerAdapter(data: [
-          NumberPickerColumn(begin: 0, end: 999),
-        ]),
-        delimiter: [
-          PickerDelimiter(child: Container(
-            width: 30.0,
-            alignment: Alignment.center,
-            child: Icon(Icons.more_vert),
-          ))
-        ],
-        hideHeader: true,
-        title: new Text("Ungefähr geschätzte Redezeit..."),
-        onConfirm: (Picker picker, List value) {
-          _minutes = _getPickerValue();        }
-    );
-  }
-
-  _onShowPicker(BuildContext context){ 
-    return _picker.showDialog(context);
-  }
-
-  int _getPickerValue() {
-    return _picker.getSelectedValues().first;
   }
 
   @override
