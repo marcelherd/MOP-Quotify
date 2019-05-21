c1dbfce1d254526a8b30f7b669aa8b1ff46a6bd7
Manuel Schwalm <manuel.schwlam@sovanta.com>
2019-05-22 01:06:06 +0200

    Adjusted Statistics to realod automatically and show beautiful!

--- app/lib/screens/session/pages/statistics.dart
+++ app/lib/screens/session/pages/statistics.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:url_launcher/url_launcher.dart';
@@ -6,13 +7,23 @@ import 'package:app/models/debate.dart';
 import 'package:app/services/debate_service.dart';
 import 'package:app/screens/home/index.dart';
 
-class StatisticsScreen extends StatelessWidget {
-  final List<charts.Series> seriesList;
-  final bool animate = false;
+class StatisticsScreen extends StatefulWidget {
   final Debate _debate;
   final Author author;
 
-  StatisticsScreen(this._debate, {this.author}) : seriesList = _createSampleData();
+  StatisticsScreen(this._debate, {this.author});
+
+  @override
+  State<StatefulWidget> createState() => _StatisticsScreenState(_debate, author: this.author);
+
+}
+
+class _StatisticsScreenState extends State<StatisticsScreen> {
+
+  final bool animate = true;
+  final Debate _debate;
+  final Author author;
+  _StatisticsScreenState(this._debate, {this.author});
 
   void _onPressExport() async {
     final url = 'https://quotify-9b7z0.firebaseapp.com/?d=${_debate.debateCode}';
@@ -52,11 +63,17 @@ class StatisticsScreen extends StatelessWidget {
       body: Container(
         padding: EdgeInsets.symmetric(vertical: 32.0, horizontal: 16.0),
         child: Center(
-            child: charts.PieChart(seriesList,
-                animate: animate,
-                defaultRenderer: charts.ArcRendererConfig(
-                    arcWidth: 100,
-                    arcRendererDecorators: [charts.ArcLabelDecorator()]))),
+            child: StreamBuilder(
+            stream: Firestore.instance
+              .collection(widget._debate.debateCode)
+              .orderBy('archived')
+              .snapshots(),
+            builder: (context, snapshot) {
+              if (!snapshot.hasData) return Text('Loading...');
+              return _fetchDataAsStream(snapshot);
+            },
+          ),
+        ),
       ),
       persistentFooterButtons: author == null ? <Widget>[
         RaisedButton(
@@ -73,30 +90,85 @@ class StatisticsScreen extends StatelessWidget {
     );
   }
 
-  /// Create one series with sample hard coded data.
-  static List<charts.Series<LinearSales, int>> _createSampleData() {
-    final data = [
-      LinearSales(0, 100),
-      LinearSales(1, 75),
-      LinearSales(2, 25),
-      LinearSales(3, 5),
-    ];
+    Widget _fetchDataAsStream(AsyncSnapshot<dynamic> snapshot) {
+    
+    Map<String, int> propertyCounter = new Map<String, int>();
+    propertyCounter["Männlich"] = 0;
+    propertyCounter["Weiblich"] = 0;
+    propertyCounter["Divers"] = 0;
+    _debate.customProperties.forEach((k, v) {
+      if(v.length > 0){
+        v.forEach((value) {
+          propertyCounter[value.toString()] = 0;
+        });
+      }else{
+        propertyCounter[k] = 0;
+        propertyCounter["Nicht " + k] = 0;
+      }
+    });
+    
+    double fullTime = 0;
+    snapshot.data.documents.forEach((DocumentSnapshot document) {
+      if(document.documentID != 'metadata'){
+        Contribution c = Contribution.fromJson(document.data, document.documentID);
+        switch(c.author.gender){
+          case Gender.male:
+            propertyCounter["Männlich"]+=c.duration.round();
+            break;
+          case Gender.female:
+            propertyCounter["Weiblich"]+=c.duration.round();
+            break;
+          case Gender.diverse:
+            propertyCounter["Divers"]+=c.duration.round();
+        }
+        c.author.customProperties.forEach((k, v) {
+          if(v is bool){
+            if(v){
+              propertyCounter[k] += c.duration.round();
+            }else{
+              propertyCounter["Nicht " + k]+=c.duration.round();
+            }
+          }else{
+            propertyCounter[v.toString()]+= c.duration.round();
+          }
+        });
+        fullTime += c.duration.round();
+      }
+    });
+    if(fullTime > 0){
+      List<QuotedContribution> data = [];
+      propertyCounter.forEach((String k, int v) {
+        data.add(QuotedContribution(k, (v / fullTime*100).round()));
+      });
+      return charts.PieChart(
+                      [
+                        charts.Series<QuotedContribution, String>(
+                        id: "Contributions", 
+                        domainFn: (QuotedContribution contrib, _) => contrib.propertyName,
+                        measureFn: (QuotedContribution contrib, _) => contrib.count,
+                        data: data,
+                        labelAccessorFn: (QuotedContribution contrib, _) => '${contrib.propertyName} - ${contrib.count}%',
+                        )
+                      ],
+                      animate: animate,
+                      defaultRenderer: new charts.ArcRendererConfig(arcRendererDecorators: [
+                        new charts.ArcLabelDecorator(
+                            labelPosition: charts.ArcLabelPosition.auto,
+                            insideLabelStyleSpec: charts.TextStyleSpec(fontSize: 9)
+                            )
+                      ]),
+                    );
+    }else {
+      return Text("Keine Wortmeldungen für die Statistik verfügbar!");
+    }
 
-    return [
-      charts.Series<LinearSales, int>(
-          id: 'Sales',
-          domainFn: (LinearSales sales, _) => sales.year,
-          measureFn: (LinearSales sales, _) => sales.sales,
-          data: data,
-          labelAccessorFn: (LinearSales row, _) => '${row.year}: ${row.sales}')
-    ];
   }
+
 }
 
-/// Sample linear data type.
-class LinearSales {
-  final int year;
-  final int sales;
+  class QuotedContribution {
+    final String propertyName;
+    final int count;
 
-  LinearSales(this.year, this.sales);
-}
+    QuotedContribution(this.propertyName, this.count);
+  }
